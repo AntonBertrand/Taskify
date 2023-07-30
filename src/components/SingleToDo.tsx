7f701d25910afdb2012d8978c8069ce150573268
Anton Bertrand <56392640+AntonBertrand@users.noreply.github.com>
2023-07-30 20:46:32 +0100

    Tasks can now be edited

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from '../model'
 import {AiFillEdit} from 'react-icons/ai'
 import {AiFillDelete} from 'react-icons/ai'
@@ -14,6 +14,9 @@ type Props = {
 
 const SingleToDo = ({todo, todos, setToDos}: Props) => {
 
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setToDos(
       todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
@@ -24,17 +27,34 @@ const SingleToDo = ({todo, todos, setToDos}: Props) => {
     setToDos(todos.filter((todo) => todo.id !== id))
   };
 
-  return (
-    <form className='todos__single'>
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
 
-    {todo.isDone ? (
-        <s className='todos__single__text'>{todo.todo}</s>
-      ) : (
-        <span className='todos__single__text'>{todo.todo}</span>
-      )}
+    setToDos(todos.map((todo) => (
+      todo.id === id? {...todo, todo:editTodo} : todo
+    )));
+
+    setEdit(false);
+  }
+
+  return (
+    <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
+
+      { edit ? (
+          <input value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='todos__single__text'/>
+        ) : todo.isDone ? (
+          <s className='todos__single__text'> {todo.todo}</s>
+         ) : (
+          <span className='todos__single__text'>{todo.todo}</span>
+         )
+      }
 
       <div>
-        <span className='icon'>
+        <span className='icon' onClick={ () =>
+          {if (!edit && !todo.isDone) {
+            setEdit(!edit)
+          }}
+        }>
           <AiFillEdit/>
         </span>
 
